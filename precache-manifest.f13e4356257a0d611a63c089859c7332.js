ecf5a66f2cfd5c8192bab3b3aeb0fc026a5f8013
Pankaj Jaiswal <pankajjaiswalcs2017@gmail.com>
2020-07-17 23:20:55 +0530

    Updates

---
+++ precache-manifest.f13e4356257a0d611a63c089859c7332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fad5157778d280ebebd3aa362f28f5",
+    "url": "/hello-reacts/index.html"
+  },
+  {
+    "revision": "7aaeb09ad68c0f965710",
+    "url": "/hello-reacts/static/css/main.c3388e84.chunk.css"
+  },
+  {
+    "revision": "e708e1a85a837dbb728b",
+    "url": "/hello-reacts/static/js/2.ebe34363.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/hello-reacts/static/js/2.ebe34363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaeb09ad68c0f965710",
+    "url": "/hello-reacts/static/js/main.322302f4.chunk.js"
+  },
+  {
+    "revision": "4e541751038c3b7fc944",
+    "url": "/hello-reacts/static/js/runtime-main.40d24ab6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-reacts/static/media/logo.5d5d9eef.svg"
+  }
+]);
